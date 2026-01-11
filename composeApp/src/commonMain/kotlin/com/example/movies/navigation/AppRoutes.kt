2894f55614e5eaf358d820702ab6787944c166af
Breno <breno.silva.2903@gmail.com>
2026-01-11 10:02:37 -0300

    feat(dependencies): Add navigation components

---
+++ composeApp/src/commonMain/kotlin/com/example/movies/navigation/AppRoutes.kt
@@ -0,0 +1,10 @@
+package com.example.movies.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface AppRoutes {
+    @Serializable
+    data object MoviesList: AppRoutes
+    @Serializable
+    data class MovieDetails(val id:Int): AppRoutes
+}
