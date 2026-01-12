cbf492ea304f5f793ecd01535a5920ec41ad7c9b
Breno <breno.silva.2903@gmail.com>
2026-01-12 19:35:22 -0300

    feat(movie): Personalization theme

---
+++ composeApp/src/commonMain/kotlin/com/example/movies/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.movies.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
+
+val Primary80 = Color(0xFFE50914)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val TextPrimaryDark = Color(0xFFFFFFFF)
+val ColorError = Color(0xFFF24E1E)
+val Neutral60 = Color(0xFF8A91A8)
+
+// Screen type(dark and light)
+internal val AppColorScheme = darkColorScheme(
+    primary = Primary80,
+    onPrimary = Color.White,
+    background = BackgroundDark,
+    onBackground = TextPrimaryDark,
+    surface = SurfaceDark,
+    onSurface = TextPrimaryDark,
+    secondary = Neutral60,
+    onSecondary = TextPrimaryDark,
+    error = ColorError,
+    onError = Color.White
+)
