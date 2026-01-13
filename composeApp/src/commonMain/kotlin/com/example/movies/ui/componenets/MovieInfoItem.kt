222e748a2f1fddcab313ab07901b302d9c66ddfd
Breno <breno.silva.2903@gmail.com>
2026-01-13 20:54:52 -0300

    feat(movie-details): Add move info component

---
+++ composeApp/src/commonMain/kotlin/com/example/movies/ui/componenets/MovieInfoItem.kt
@@ -0,0 +1,45 @@
+package com.example.movies.ui.componenets
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.movies.ui.theme.MoviesAppTheme
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Star
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun MovieInfoItemPreview() {
+    MoviesAppTheme {
+        MovieInfoItem(icon = FontAwesomeIcons.Solid.Star, text = "7.5")
+    }
+}
+
+@Composable
+fun MovieInfoItem(modifier: Modifier = Modifier, icon: ImageVector, text: String) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            modifier = Modifier.size(10.dp),
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.Gray
+        )
+        Text(
+            modifier = Modifier.padding(start = 2.dp),
+            text = text,
+            color = Color.Gray,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
